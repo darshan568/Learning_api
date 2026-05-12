1d980037e8a7190908ad1dd1bafd092876b13c6f
DARSHAN S <darshan.sathish@cognizant.com>
2026-05-12 23:48:41 +0530

    HttpGetById done

--- learning_api1/learning_api1/Controllers/EmployeeController.cs
+++ learning_api1/learning_api1/Controllers/EmployeeController.cs
@@ -12,7 +12,8 @@ namespace Learning_api1.Controllers
     //inheriting controllerbase class powers    //sometimes we use controller instead of controllerbase (controller also inherites from controllerbase and has additional features)
     {
         //creating List(vector) name is _emp
-        public static List<string> _emp = new List<string>();
+        //public static List<string> _emp = new List<string>();
+        public static List<Employee> _emp = new List<Employee>();
 
 
         [HttpGet]
@@ -21,6 +22,7 @@ namespace Learning_api1.Controllers
         public IActionResult GetEmployees()
         {
             return Ok(_emp);
+
         }
 
         [HttpPost]
@@ -36,16 +38,41 @@ namespace Learning_api1.Controllers
         //    return Created("", _emp);
         //}
 
-        public IActionResult AddEmployee(Employee emp)
+        public IActionResult AddEmployee(int id, string name, double salary)
         {
-            if (string.IsNullOrWhiteSpace(emp.Name) || emp.Id <= 0 || emp.Salary <= 0)
+            if (string.IsNullOrWhiteSpace(name) || id <= 0 || salary <= 0)
             {
                 return BadRequest("Invalid input. Please provide a valid name, Id, and salary.");
             }
-            _emp.Add(emp.Name);
-            _emp.Add(emp.Id.ToString());
-            _emp.Add(emp.Salary.ToString());
+            Employee newEmplbox = new Employee
+            {
+                Id = id,
+                Name = name,
+                Salary = salary
+            };
+            _emp.Add(newEmplbox);
             return Created("", _emp);
+        }
+
+       
+
+        [HttpGet("{id}")]
+        public IActionResult GetEmployeeById(int id)
+        {
+            //if(id<=0)
+            //{
+            //    return BadRequest("Invalid input. Please provide a valid Id.");
+            //}
+            //return Ok(_emp);
+            var ee = _emp.FirstOrDefault(e => e.Id == id);   //lamda  , this Id is from class 
+            //FirstOrDefault - 1)First: compiler will start from 1st index compares the id if true returns it and exit . else gives 400
+                                //2)OrDefault: inorder to avoid 400 we use this along with First ->if true returns it and exit . else gives null
+            if (ee == null)
+            {
+                return NotFound("Employee not found.");
+            }
+            return Ok(ee);
+
         }
     }
 }
